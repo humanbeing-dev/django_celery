18bf32c4da62dbee869eef2a162c4dcf9f190891
maciej.sitowski <maciej.sitowski@gmail.com>
2023-04-18 08:00:16 +0200

    tst: Add factory boy and coverage

--- polls/tests.py
+++ polls/tests.py
@@ -4,6 +4,7 @@ from celery.exceptions import Retry
 from django.contrib.auth.models import User
 from django.test import TestCase, TransactionTestCase, override_settings
 from django.urls import reverse
+from polls.factories import UserFactory
 
 from polls.tasks import task_add_subscribe
 
@@ -32,7 +33,7 @@ class TaskAddSubscribeTest(TestCase):
 
     @patch("polls.tasks.requests.post")
     def test_post_succeed(self, mock_requests_post):
-        instance = User.objects.create(username="test", email="test@gmail.com")
+        instance = UserFactory.create()
         task_add_subscribe(instance.pk)
 
         mock_requests_post.assert_called_with(
@@ -45,7 +46,7 @@ class TaskAddSubscribeTest(TestCase):
         mock_requests_post.side_effect = Exception()
         mock_task_add_subscribe_retry.side_effect = Retry()
 
-        instance = User.objects.create(username="test", email="test@gmail.com")
+        instance = UserFactory.create()
 
         with self.assertRaises(Retry):
             task_add_subscribe(instance.pk)
